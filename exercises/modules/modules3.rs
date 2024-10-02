f582c3f1365992afc3dbccb9e4c7f2954cedbf55
yuanshen <z20230404@gmail.com>
2024-10-02 14:38:50 +0800

    Update

--- exercises/modules/modules3.rs
+++ exercises/modules/modules3.rs
@@ -10,11 +10,11 @@
 
 
 // TODO: Complete this use statement
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::time;
 
 
 fn main() {
-    match SystemTime::now().duration_since(UNIX_EPOCH) {
+    match time::SystemTime::now().duration_since(time::UNIX_EPOCH) {
         Ok(n) => println!("1970-01-01 00:00:00 UTC was {} seconds ago!", n.as_secs()),
         Err(_) => panic!("SystemTime before UNIX EPOCH!"),
     }
